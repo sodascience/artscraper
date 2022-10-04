4a7237fc75b9ef8c31b7bd8bcbcc2e14e6d079e5
Javier Garcia-Bernardo <j.garciabernardo@uu.nl>
2022-10-04 14:27:00 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 
 setup(
     name='artscraper',
-    version='0.1.0',
+    version='0.1.1',
     author='ODISSEI Social Data Science Team',
     description='Package for scraping artworks from WikiArt and GoogleArt',
     long_description=readme(),
