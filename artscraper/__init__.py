0e48508425f1825a006b05d8460a9596ffd058fe
Modhurita Mitra <3924929+modhurita@users.noreply.github.com>
2023-03-29 15:57:06 +0200

    Add method to extract artist links from googlearts

--- artscraper/__init__.py
+++ artscraper/__init__.py
@@ -1,6 +1,7 @@
 """Scrape art image and metadata from WikiArt and Google Arts."""
 
-from artscraper.googleart import GoogleArtScraper
+from artscraper.googleart import GoogleArtScraper, random_wait_time
 from artscraper.wikiart import WikiArtScraper
+from artscraper.artist_links import get_artist_links
 
-__all__ = ["GoogleArtScraper", "WikiArtScraper"]
+__all__ = ["GoogleArtScraper", "WikiArtScraper", "get_artist_links", "random_wait_time"]
---
+++ artscraper/artist_links.py
@@ -0,0 +1,50 @@
+'''
+Get artist links from Google Arts & Culture webpage
+'''
+
+import time
+from selenium import webdriver
+from artscraper.googleart import random_wait_time
+
+
+def get_artist_links(webpage):
+    '''
+    Parameters
+    ----------
+    webpage : Web address of Google Arts & Culture artists page
+
+    Returns
+    -------
+    list_links : List with Google Arts & Culture web addresses for all artists
+    '''
+
+    # Launch Firefox browser
+    driver = webdriver.Firefox()
+
+    # Get Google Arts & Culture webpage listing all artists
+    driver.get(webpage)
+
+    # Get scroll height after first time page load
+    last_height = driver.execute_script("return document.body.scrollHeight")
+    while True:
+        # Scroll down to bottom
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        # Wait to load page
+        time.sleep(random_wait_time(min_wait=1))
+        # Calculate new scroll height and compare with last scroll height
+        new_height = driver.execute_script("return document.body.scrollHeight")
+        if new_height == last_height:
+            break
+        last_height = new_height
+
+    # Find xpaths containing artist links
+    elements = driver.find_elements('xpath', '//*[contains(@href,"categoryId=artist")]')
+
+    # List to store artist links
+    list_links = []
+    # Go through each xpath containing an artist link
+    for element in elements:
+        # Append to list
+        list_links.append(element.get_attribute('href'))
+
+    return list_links
