0f61262491acbc62e30ccfba9b44e89f6ee56d68
´Modhurita <modhurita@gmail.com>
2023-03-29 14:07:55 +0200

    fix some pylint errors

--- artscraper/artist_links.py
+++ artscraper/artist_links.py
@@ -1,13 +1,26 @@
+'''
+Get artist links from Google Arts & Culture webpage
+'''
+
+import time
 from selenium import webdriver
 from artscraper import random_wait_time
-import time
 
 
 def get_artist_links(webpage):
-    
+    '''
+    Parameters
+    ----------
+    webpage : Web address of Google Arts & Culture artists page
+
+    Returns
+    -------
+    list_links : List with Google Arts & Culture web addresses for all artists
+    '''
+
     # Launch Firefox browser
     driver = webdriver.Firefox()
-    
+
     # Get Google Arts & Culture webpage listing all artists
     driver.get(webpage)
 
@@ -16,7 +29,7 @@ def get_artist_links(webpage):
     while True:
         # Scroll down to bottom
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        # Wait to load page 
+        # Wait to load page
         time.sleep(random_wait_time(min_wait=1))
         # Calculate new scroll height and compare with last scroll height
         new_height = driver.execute_script("return document.body.scrollHeight")
@@ -26,7 +39,7 @@ def get_artist_links(webpage):
 
     # Find xpaths containing artist links
     elements = driver.find_elements('xpath', '//*[contains(@href,"categoryId=artist")]')
-    
+
     # List to store artist links
     list_links = []
     # Go through each xpath containing an artist link
