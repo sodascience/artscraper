ece92e12386fd65fd8733b4b66aa351784aeabbb
´Modhurita <modhurita@gmail.com>
2023-04-03 16:00:51 +0200

    remove \

--- artscraper/__init__.py
+++ artscraper/__init__.py
@@ -5,5 +5,5 @@ from artscraper.wikiart import WikiArtScraper
 from artscraper.artist_links import get_artist_links
 from artscraper.artist_works_metadata import get_artist_works
 
-__all__ = ["GoogleArtScraper", "WikiArtScraper", \
+__all__ = ["GoogleArtScraper", "WikiArtScraper",
            "get_artist_links", "get_artist_works", "random_wait_time"]
