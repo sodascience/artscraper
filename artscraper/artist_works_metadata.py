0d9d820966930686cfb75bcb6dad5bf865281429
´Modhurita <modhurita@gmail.com>
2023-04-04 10:27:43 +0200

    add function to get artist description

--- artscraper/artist_works_metadata.py
+++ artscraper/artist_works_metadata.py
@@ -1,9 +1,11 @@
 '''
-Get artist links from Google Arts & Culture webpage
+get_artist_works: Get artist links from Google Arts & Culture webpage
+get _artist_description: Get description of the artist, from Wikipedia
 '''
 
 import time
 from selenium import webdriver
+import wikipediaapi
 from artscraper.googleart import random_wait_time
 
 
@@ -17,7 +19,7 @@ def get_artist_works(artist_link, min_wait_time):
 
     Returns
     -------
-    list_works: List with web addresses of the artist's works on Google Arts & Culture
+    list_links: List with web addresses of the artist's works on Google Arts & Culture
 
     '''
 
@@ -58,3 +60,38 @@ def get_artist_works(artist_link, min_wait_time):
     list_links = [element.get_attribute('href') for element in elements]
 
     return list_links
+
+def get_artist_description(artist_link):
+
+    '''
+    Parameters
+    ----------
+    artist_link : Web address of the artist's Google Arts & Culture page
+
+    Returns
+    -------
+    description : Description of artist (lead section of Wikipedia article)
+
+    '''
+
+    # Launch Firefox browser
+    driver = webdriver.Firefox()
+
+    # Get Google Arts & Culture webpage for the artist
+    driver.get(artist_link)
+
+    # Locate the element containing the link to the artist's Wikipedia article
+    element = driver.find_element('xpath','//*[contains(@href,"wikipedia")]')
+    # Extract the link to the Wikipedia article
+    wikipedia_link = element.get_attribute('href')
+    # Find the title of the Wikipedia article
+    title = wikipedia_link.rsplit('/')[-1]
+
+    # Choose the English Wikipedia
+    wiki = wikipediaapi.Wikipedia('en')
+    # Get the Wikipedia page
+    page = wiki.page(title)
+    # Get summary of the page (lead section of the Wikipedia article)
+    description = page.summary
+
+    return description
