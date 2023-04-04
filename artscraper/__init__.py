8deb511f8b6a5ab2d3792d9695fb1b01a1ac22f4
Modhurita Mitra <3924929+modhurita@users.noreply.github.com>
2023-04-04 14:45:30 +0200

    Add function to extract the description of an artist from Wikipedia

--- artscraper/__init__.py
+++ artscraper/__init__.py
@@ -3,7 +3,8 @@
 from artscraper.googleart import GoogleArtScraper, random_wait_time
 from artscraper.wikiart import WikiArtScraper
 from artscraper.artist_links import get_artist_links
-from artscraper.artist_works_metadata import get_artist_works
+from artscraper.artist_works_metadata import get_artist_works, get_artist_description
 
 __all__ = ["GoogleArtScraper", "WikiArtScraper",
-           "get_artist_links", "get_artist_works", "random_wait_time"]
+           "get_artist_links", "get_artist_works",
+           "get_artist_description", "random_wait_time"]
