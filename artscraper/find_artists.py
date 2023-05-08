7f0760778aa87379128fd46de536b0dd45334ce9
Modhurita Mitra <3924929+modhurita@users.noreply.github.com>
2023-05-08 11:13:31 +0200

    Feedback incorporated (#13)
    * add function to extract metadata from wikidata
    
    * add functions for extracting metadata from wikidata
    
    * get artist metadata
    
    * SPARQL query to fetch metadata from wikidata
    
    * fix pylint error about missing timeout argument
    
    * remove txt file with SPARQL query
    
    * add SPARQL query as global string
    
    * display metadata with unrounded coordinates
    
    * Update artscraper/__init__.py
    
    Co-authored-by: qubixes <44498096+qubixes@users.noreply.github.com>
    
    * make SPARQL query more readable
    
    * update examples notebook
    
    * rename files
    
    * change from functions to class
    
    * describe how to obtain all artworks
    
    * transform from functions to class
    
    * illustrate how to collect all artworks
    
    * put random wait time function in separate file
    
    * remove old example notebooks
    
    ---------
    
    Co-authored-by: ´Modhurita <´modhurita@gmail.com´>
    Co-authored-by: qubixes <44498096+qubixes@users.noreply.github.com>

--- artscraper/find_artists.py
+++ artscraper/find_artists.py
@@ -4,14 +4,17 @@ Get artist links from Google Arts & Culture webpage
 
 import time
 from selenium import webdriver
-from artscraper.googleart import random_wait_time
+from artscraper.functions import random_wait_time
 
-
-def get_artist_links(webpage):
+def get_artist_links(webpage='https://artsandculture.google.com/category/artist',
+                     executable_path='geckodriver',
+                     min_wait_time=5, output_file=None):
     '''
     Parameters
     ----------
     webpage : Web address of Google Arts & Culture artists page
+    executable_path: Path to geckodriver
+    output_file: File to which the list of links is to be written
 
     Returns
     -------
@@ -19,7 +22,7 @@ def get_artist_links(webpage):
     '''
 
     # Launch Firefox browser
-    driver = webdriver.Firefox()
+    driver = webdriver.Firefox(executable_path=executable_path)
 
     # Get Google Arts & Culture webpage listing all artists
     driver.get(webpage)
@@ -30,7 +33,7 @@ def get_artist_links(webpage):
         # Scroll down to bottom
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
         # Wait to load page
-        time.sleep(random_wait_time(min_wait=1))
+        time.sleep(random_wait_time(min_wait=min_wait_time))
         # Calculate new scroll height and compare with last scroll height
         new_height = driver.execute_script("return document.body.scrollHeight")
         if new_height == last_height:
@@ -44,7 +47,20 @@ def get_artist_links(webpage):
     list_links = []
     # Go through each xpath containing an artist link
     for element in elements:
+        # Extract link to webpage
+        link = element.get_attribute('href')
+        # Remove trailing text
+        link = link.replace('?categoryId=artist', '')
         # Append to list
-        list_links.append(element.get_attribute('href'))
+        list_links.append(link)
+
+    # Close driver
+    driver.close()
+
+    if output_file:
+        with open(output_file, 'w', encoding='utf-8') as file:
+            for link in list_links:
+                file.write(link)
+                file.write('\n')
 
     return list_links
